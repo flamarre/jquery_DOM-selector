5384730bca999aa638cdd91989f783a0a2c94e11
flamarre <flamarre@users.noreply.github.com>
2018-03-04 15:16:53 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready function() {
+	$("#readLess").click(function () {
+		$(".column").slideUp();
+	});
+	$(document).ready function() {
+	$("#readMore").click(function () {
+		$(".column").slideDown();
+	});
+
+	function() {
+		$("#readLess").hide();
+	}
+	function() {
+		$("#readLess").show();
+	}
